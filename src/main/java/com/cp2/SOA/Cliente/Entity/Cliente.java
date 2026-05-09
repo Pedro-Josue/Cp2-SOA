6ced3b5e16ba39395f6c2d26c79c1be6597cfffc
Pedro Josué <pedro.josue.ap@outlook.com>
2026-05-09 00:25:49 -0300

    Estrutura inicial do projeto e dominio de Usuario modelado.

---
+++ src/main/java/com/cp2/SOA/Cliente/Entity/Cliente.java
@@ -0,0 +1,24 @@
+package com.cp2.SOA.Cliente.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "cliente")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(nullable = false, length = 150)
+    private String email;
+}
---
+++ src/test/java/com/cp2/SOA/SoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cp2.SOA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
